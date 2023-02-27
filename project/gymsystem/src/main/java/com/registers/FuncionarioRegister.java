a7ed71a5c1e8f90de732ab4edc87710b46afa8bd
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2023-02-26 23:26:48 -0300

    fix: erro de imports no spring

--- project/gymsystem/src/main/java/com/registers/FuncionarioRegister.java
+++ project/gymsystem/src/main/java/com/registers/FuncionarioRegister.java
@@ -1,11 +1,13 @@
 package com.registers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.entities.Funcionario;
+import com.repositories.FuncionarioRepository;
 
 @Service
 public class FuncionarioRegister {
