de0ea97f00be50a1a9841e208861776fd0341969
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2023-02-28 02:48:12 -0300

    feature: add remove and update

--- project/gymsystem/src/main/java/com/controllers/FuncionariosController.java
+++ project/gymsystem/src/main/java/com/controllers/FuncionariosController.java
@@ -2,8 +2,12 @@ package com.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +21,7 @@ import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/funcionarios")
+@CrossOrigin(origins = "http://localhost:4200")
 class FuncionariosController {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
@@ -29,19 +34,33 @@ class FuncionariosController {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Funcionario postFuncionario(@RequestBody Funcionario funcionario) {
+        System.out.println(funcionario);
         return funcionarioRepository.save(funcionario);
     }
 
+    @PutMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Funcionario putFuncionario(@RequestBody Funcionario funcionario) {
+        funcionarioRepository.delete(funcionario);
+        return funcionarioRepository.save(funcionario);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteFuncionario(@RequestBody Funcionario funcionario) {
+        funcionarioRepository.delete(funcionario);
+    }
+
     @PostMapping("/login")
-    public String login(@RequestParam("cpf") String cpf,
+    public ResponseEntity<String> login(@RequestParam("cpf") String cpf,
             @RequestParam("password") String password,
             HttpSession session) {
         Funcionario funcionario = funcionarioRepository.findFuncionarioByCpf(cpf);
         if (funcionario != null && funcionario.getPassword().equals(password)) {
             session.setAttribute("funcionario", funcionario);
-            return "redirect:/funcionario/home";
+            return ResponseEntity.status(HttpStatus.CREATED).body("redirect:/funcionario/home");
         } else {
-            return "redirect:/funcionario/login?error";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid login credentials");
         }
     }
 
