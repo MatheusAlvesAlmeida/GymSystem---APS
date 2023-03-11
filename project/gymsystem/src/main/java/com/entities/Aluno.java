c52e0a03363467bd75b6632f84f68cdc5b9e6361
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2023-03-11 13:11:53 -0300

    chore: remove register

--- project/gymsystem/src/main/java/com/entities/Aluno.java
+++ project/gymsystem/src/main/java/com/entities/Aluno.java
@@ -1,12 +1,8 @@
 package com.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.services.Observer;
-
 import jakarta.persistence.*;
 
+@Entity
 @Table(name = "alunos")
 
 public class Aluno extends Usuario {
@@ -16,42 +12,29 @@ public class Aluno extends Usuario {
     private String dataInicio;
     @Column(name = "treino")
     private String treino;
-    @Transient
-    private List<Observer> observers = new ArrayList<>();
-
 
     // Getters and Setters
     public String getPlano() {
         return plano;
     }
+
     public void setPlano(String plano) {
         this.plano = plano;
     }
+
     public String getDataInicio() {
         return dataInicio;
     }
+
     public void setDataInicio(String dataInicio) {
         this.dataInicio = dataInicio;
     }
+
     public String getTreino() {
-        return treino;        
+        return treino;
     }
+
     public void setTreino(String treino) {
         this.treino = treino;
-        notifyObservers();
-    }
-
-    public void attach(Observer observer) {
-        observers.add(observer);
-    }
-
-    public void detach(Observer observer) {
-        observers.remove(observer);
-    }
-
-    public void notifyObservers() {
-        for (Observer observer : observers) {
-            observer.update(this);
-        }
     }
 }
