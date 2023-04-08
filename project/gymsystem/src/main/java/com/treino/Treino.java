48c9e581dd8e9432a488f203ee76ff9fced2b889
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2023-04-08 16:37:54 -0300

    feature: add id getter and setter, fix entity relationship with Aluno entity

--- project/gymsystem/src/main/java/com/treino/Treino.java
+++ project/gymsystem/src/main/java/com/treino/Treino.java
@@ -1,11 +1,19 @@
 package com.treino;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aluno.Aluno;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,18 +24,27 @@ public class Treino {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @Column(name = "alunos")
-    @OneToOne(mappedBy = "treino")
-    private String alunoID;
+    @OneToMany(mappedBy = "treino", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Aluno> alunos = new ArrayList<>();
     @Column(name = "descricao")
     private String descricao;
     @Column(name = "tipo")
     private String tipo;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String descricao){
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
@@ -39,11 +56,11 @@ public class Treino {
         this.tipo = tipo;
     }
 
-    public String getAlunoID() {
-        return alunoID;
+    public List<Aluno> getAlunos() {
+        return alunos;
     }
 
-    public void setAlunoID(String alunoID) {
-        this.alunoID = alunoID;
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
     }
 }
